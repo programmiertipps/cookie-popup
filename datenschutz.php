abff4f0e19a79e41299fcdda7ed45043e7fbb90b
Programmier Tipps <kontakt@programmier-tipps.de>
2019-01-18 22:25:21 +0100

    Kleinere Optimierungen und Fehlerbehebungen.

--- datenschutz.php
+++ datenschutz.php
@@ -9,7 +9,7 @@ if( isset($_COOKIE['setCookieHinweis']) ) {
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Cookie Popup</title>
+<title>Cookie Popup - Datenschutz</title>
 <link href="css/main.css" rel="stylesheet" type="text/css">
 <script type="text/javascript" src="js/custom.js"></script>
 </head>
@@ -23,7 +23,7 @@ if( isset($_COOKIE['setCookieHinweis']) ) {
 			<p>Wir verwenden Cookies. Durch die weitere Nutzung der Webseite stimmen Sie der Verwendung von Cookies zu.</p>
 		</div> 
 		<span class="more">
-			<a href="datenschutz.php">Details</a>
+			<a href="/datenschutz.php">Details</a>
 		</span>
 		<button onclick='cookieOk()'>OK, ich bin einverstanden.</button>
 	</div>
@@ -31,8 +31,9 @@ if( isset($_COOKIE['setCookieHinweis']) ) {
 <?php  }; ?>
 
 <div id="page-wrapper">
-	<h1>Cookie Popup</h1>
+	<h1>Datenschutz</h1>
 	<p>Hier kommt dein Datenschutztext hin</p>
+	<p><a href="index.php">Zurück zur Startseite</a></p>
 </div>
 
 </body>
--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,11 @@
 function cookieOk() {
-  var now = new Date(); // Variable für aktuelles Datum
-  var lifetime = now.getTime(); // Variable für Millisekunden seit 1970 bis aktuelles Datum
-  var deleteCookie = lifetime + 2592000000; // Macht den Cookie 30 Tage gültig.
+  var now = new Date(); // Aktuelles Datum
+  var lifetime = now.getTime(); // Millisekunden seit 1970 bis heute
+  var deleteCookie = lifetime + 2592000000; // Cookie wird nach 30 Tagen automatisch gelöscht
 	
   now.setTime(deleteCookie);
   var enddate = now.toUTCString();
 	
   document.cookie = "setCookieHinweis = set; path=/; secure; expires=" + enddate;
-  document.getElementById("cookie-popup").style.display = "none";
+  document.getElementById("cookie-popup").classList.add("hidden");
 }
